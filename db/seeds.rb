9648c9f8994a7e065f4e99f5649dacc0de2d7800
Lennart Jensen <lennjen@gmail.com>
2020-11-13 16:56:30 +0100

    json seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts 'Creating ingredients ...'
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'ice')
-Ingredient.create(name: 'mint leaves')
-puts 'Ingredients created.'
+# puts 'Creating ingredients ...'
+# Ingredient.create(name: 'lemon')
+# Ingredient.create(name: 'ice')
+# Ingredient.create(name: 'mint leaves')
+# puts 'Ingredients created.'
+#
+# puts 'Creating cocktails ...'
+# Cocktail.create(name: 'Negroni', image_url: 'https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg')
+# Cocktail.create(name: 'Cosmopolitan', image_url: 'https://www.thecocktaildb.com/images/media/drink/kpsajh1504368362.jpg')
+# Cocktail.create(name: 'Sex On The Beach', image_url: 'https://www.thecocktaildb.com/images/media/drink/lijtw51551455287.jpg')
+# puts 'Cocktails created.'
 
-puts 'Creating cocktails ...'
-Cocktail.create(name: 'Negroni', image_url: 'https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg')
-Cocktail.create(name: 'Cosmopolitan', image_url: 'https://www.thecocktaildb.com/images/media/drink/kpsajh1504368362.jpg')
-Cocktail.create(name: 'Sex On The Beach', image_url: 'https://www.thecocktaildb.com/images/media/drink/lijtw51551455287.jpg')
-puts 'Cocktails created.'
+require 'json'
+require 'open-uri'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+list_serialized = open(url).read
+ingredients = JSON.parse(list_serialized)
+list = ingredients['drinks']
+puts 'Cleaning database...'
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+sleep 1
+puts 'Creating data...'
+list.each do |ingredient|
+  zutat = Ingredient.new(name: ingredient['strIngredient1'])
+  zutat.save
+end
+sleep 2
+puts "Finished! #{Ingredient.count} - ingredients created"
