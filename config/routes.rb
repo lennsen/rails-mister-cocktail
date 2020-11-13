179ed58dc443503a25cecc6b328d8104dfa3a7a4
Lennart Jensen <lennjen@gmail.com>
2020-11-13 16:11:47 +0100

    mister cocktail progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get "cocktails"
+  # get "cocktails/:id"
+  # get "cocktails/new"
+  # post "cocktails"
+
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating ingredients ...'
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+puts 'Ingredients created.'
+
+puts 'Creating cocktails ...'
+Cocktail.create(name: 'Negroni', image_url: 'https://www.thecocktaildb.com/images/media/drink/qgdu971561574065.jpg')
+Cocktail.create(name: 'Cosmopolitan', image_url: 'https://www.thecocktaildb.com/images/media/drink/kpsajh1504368362.jpg')
+Cocktail.create(name: 'Sex On The Beach', image_url: 'https://www.thecocktaildb.com/images/media/drink/lijtw51551455287.jpg')
+puts 'Cocktails created.'
